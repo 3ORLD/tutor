a4746841d87207ee6d8f433c557280f2cd48eaa6
3ORLD <127151709+3ORLD@users.noreply.github.com>
2023-04-30 03:50:19 +0100

    Update tutorial.cs

--- ConsoleApp2/ConsoleApp2/tutorial.cs
+++ ConsoleApp2/ConsoleApp2/tutorial.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,12 @@ namespace ConsoleApp2
         public static void ShowInstructions()
         {
             Console.WriteLine("MathsTutor Instructions:");
-            Console.WriteLine("1. Choose 'Deal 3 cards' to generate a math problem with 2 numbers and 1 operator.");
-            Console.WriteLine("2. Enter your answer to the problem.");
-            Console.WriteLine("3. The application will tell you if your answer is correct or not.");
-            Console.WriteLine("4. Choose 'Quit' to exit the application.");
+            Console.WriteLine("Disclaimer: You can use a calculator to work out the answer");
+            Console.WriteLine("1. To begin with choose the 'Deal 3 cards' option to generate a math problem with 2 numbers and 1 Math Signs.");
+            Console.WriteLine("2. To challenge yourself, choose 'Deal 5 cards' option to generate a math problem with 3 numbers and 2 Maths Signs.");
+            Console.WriteLine("3. Enter your answer to the problem.");
+            Console.WriteLine("4. The application will tell you if your answer is correct or not.");
+            Console.WriteLine("5. Choose 'Quit' to exit the application.");
         }
     }
 
