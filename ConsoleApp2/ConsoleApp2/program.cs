62edf3cc5cc6c426022c7633c6309d2517c0bf84
3ORLD <127151709+3ORLD@users.noreply.github.com>
2023-04-30 03:49:20 +0100

    Update program.cs

--- ConsoleApp2/ConsoleApp2/program.cs
+++ ConsoleApp2/ConsoleApp2/program.cs
@@ -1,8 +1,14 @@
-﻿using ConsoleApp2;
+using ConsoleApp2;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 class Program
 {
+    private static string statisticsFilePath = "statistics.txt";
+
+    public static string StatisticsFilePath { get => statisticsFilePath; set => statisticsFilePath = value; }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to MathsTutor!");
@@ -13,7 +19,8 @@ class Program
             Console.WriteLine("\nMenu:");
             Console.WriteLine("1. Instructions");
             Console.WriteLine("2. Deal 3 cards");
-            Console.WriteLine("3. Quit");
+            Console.WriteLine("3. Deal 5 cards");
+            Console.WriteLine("4. Quit");
             Console.Write("Enter your choice: ");
             int choice = int.Parse(Console.ReadLine());
 
@@ -26,6 +33,9 @@ class Program
                     DealThreeCards();
                     break;
                 case 3:
+                    DealFiveCards();
+                    break;
+                case 4:
                     keepRunning = false;
                     break;
                 default:
@@ -34,34 +44,85 @@ class Program
             }
         }
 
+
         Console.WriteLine("\nGoodbye!");
     }
 
     private static void DealThreeCards()
     {
+
+        {
+            Pack pack = new Pack();
+            Card card1 = pack.DrawCard();
+            Card card2 = pack.DrawCard();
+            Card card3 = pack.DrawCard();
+
+            char operatorChar = GetOperatorFromSuit(card2.Suit);
+
+            Console.WriteLine($"Problem: {card1.Value} {operatorChar} {card3.Value}");
+            Console.Write("Enter your answer: ");
+            double answer = double.Parse(Console.ReadLine());
+
+            double correctAnswer = CalculateAnswer(card1.Value, card3.Value, operatorChar);
+
+            if (Math.Abs(answer - correctAnswer) < 0.001)
+            {
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine($"Incorrect. The correct answer is {correctAnswer}.");
+            }
+        }
+    }
+
+    private static void DealFiveCards()
+    {
         Pack pack = new Pack();
         Card card1 = pack.DrawCard();
         Card card2 = pack.DrawCard();
         Card card3 = pack.DrawCard();
+        Card card4 = pack.DrawCard();
+        Card card5 = pack.DrawCard();
 
-        char operatorChar = GetOperatorFromSuit(card2.Suit);
+        char operatorChar1 = GetOperatorFromSuit(card2.Suit);
+        char operatorChar2 = GetOperatorFromSuit(card4.Suit);
 
-        Console.WriteLine($"Problem: {card1.Value} {operatorChar} {card3.Value}");
+        Console.WriteLine($"Problem: {card1.Value} {operatorChar1} {card3.Value} {operatorChar2} {card5.Value}");
         Console.Write("Enter your answer: ");
         double answer = double.Parse(Console.ReadLine());
 
-        double correctAnswer = CalculateAnswer(card1.Value, card3.Value, operatorChar);
+        double correctAnswer = CalculateBODMASAnswer(card1.Value, card3.Value, card5.Value, operatorChar1, operatorChar2);
 
         if (Math.Abs(answer - correctAnswer) < 0.001)
         {
             Console.WriteLine("Correct!");
+
         }
         else
         {
             Console.WriteLine($"Incorrect. The correct answer is {correctAnswer}.");
+
         }
     }
 
+    // Existing code for GetOperatorFromSuit and CalculateAnswer
+
+    private static double CalculateBODMASAnswer(int value1, int value2, int value3, char operatorChar1, char operatorChar2)
+    {
+        double intermediateValue;
+
+        if ((operatorChar1 == '*' || operatorChar1 == '/') && (operatorChar2 == '+' || operatorChar2 == '-'))
+        {
+            intermediateValue = CalculateAnswer(value1, value2, operatorChar1);
+            return CalculateAnswer((int)Math.Round(intermediateValue), value3, operatorChar2);
+        }
+        else
+        {
+            intermediateValue = CalculateAnswer(value2, value3, operatorChar2);
+            return CalculateAnswer(value1, (int)Math.Round(intermediateValue), operatorChar1);
+        }
+    }
     private static char GetOperatorFromSuit(int suit)
     {
         return suit switch
