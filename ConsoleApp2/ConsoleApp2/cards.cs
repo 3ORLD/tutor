67237c7eb62a773fa858846b24534e54d09f10b8
3ORLD <127151709+3ORLD@users.noreply.github.com>
2023-04-30 05:14:33 +0100

    Update cards.cs

--- ConsoleApp2/ConsoleApp2/cards.cs
+++ ConsoleApp2/ConsoleApp2/cards.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,17 @@ namespace ConsoleApp2
     {
         public int Value { get; set; }
         public int Suit { get; set; }
-    }
 
+        public virtual string GetDescription()
+        {
+            return $"Value: {Value}, Suit: {Suit}";
+        }
+    }
+    public class SpecialCard : Card
+    {
+        public override string GetDescription()
+        {
+            return $"SPECIAL CARD - Value: {Value}, Suit: {Suit}";
+        }
+    }
 }
